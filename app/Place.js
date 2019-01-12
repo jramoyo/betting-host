9a76f4abdf45a7cc14133a65c84e1dec5fc592df
Jan Amoyo <jramoyo@gmail.com>
2019-01-12 14:44:26 +1100

    adds Place and its test

---
+++ app/Place.js
@@ -0,0 +1,25 @@
+const roundTo = require('round-to');
+
+const COMMISSION = .12;
+module.exports = class {
+  constructor() {
+    this.total = 0;
+    this.pool = {};
+  }
+
+  bet(selection, stake) {
+    this.total += stake;
+    this.pool[selection] = this.pool[selection] || [];
+    this.pool[selection].push(stake);
+  }
+
+  result(selection) {
+    if (!this.pool[selection]) {
+      return 0
+    }
+
+    const pot = (this.total - (this.total * COMMISSION)) / 3;
+    const shares = this.pool[selection].reduce((total, stake) => total + stake, 0);
+    return roundTo((pot / shares), 2);
+  }
+};
---
+++ tests/Place.spec.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const Place = require('../app/Place');
+
+const expect = chai.expect;
+
+describe('Place', () => {
+
+  let place;
+
+  beforeEach(() => {
+    place = new Place()
+  })
+
+  it('calculates the dividends with 2 decimal places', () => {
+
+    place.bet(1, 31);
+    place.bet(2, 89);
+    place.bet(3, 28);
+    place.bet(4, 72);
+    place.bet(1, 40);
+    place.bet(2, 16);
+    place.bet(3, 82);
+    place.bet(4, 52);
+    place.bet(1, 18);
+    place.bet(2, 74);
+    place.bet(3, 39);
+    place.bet(4, 105);
+
+    expect(place.result(2)).to.equal(1.06);
+    expect(place.result(3)).to.equal(1.27);
+    expect(place.result(1)).to.equal(2.13);
+
+  });
+
+  it('calculates a zero dividend if there are no bets on the winning stake', () => {
+    place.bet(1, 10);
+    expect(place.result(2)).to.equal(0);
+  });
+
+});
